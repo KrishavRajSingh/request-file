babb9a5e98a10fb0b3da794845cb2bd0ed477268
Krishav Raj Singh <krishavrajsingh@gmail.com>
2025-05-25 00:51:31 +0530

    added endpoint

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
-
+import os
 app = Flask(__name__)
 
-@app.route('/save', methods=['POST'])
+@app.route('/', methods=['POST'])
+@app.route('/hdata.aspx', methods=['POST'])
 def save_request():
     data = request.get_data(as_text=True)
     with open('requests.txt', 'a') as f:
@@ -11,9 +12,12 @@ def save_request():
 
 @app.route('/getData', methods=['GET'])
 def get_data():
+    if not os.path.exists('requests.txt'):
+        return "No data found", 404
     with open('requests.txt', 'r') as f:
         data = f.read()
-    return data, 200
+        return data, 200
+    return 'error occured', 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
